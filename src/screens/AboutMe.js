b3b213762f2e8a370c42dbf57894896fd4e5cec2
Victor Kang Kai Sheng <66403541+victorkang516@users.noreply.github.com>
2024-04-30 00:16:21 +0800

    Update overall UI

--- src/screens/AboutMe.js
+++ src/screens/AboutMe.js
@@ -27,11 +27,11 @@ const AboutMe = () => {
     return (
         <section className={`about-me ${aboutMeIsVisible ? 'fadeIn' : ''}`} ref={aboutMeRef} id='about-me'>
             <div className='title'>
-                <h2>SOFTWARE ENGINEER</h2>
+                <h2>SOFTWARE ENGINEER | UIUX DESIGNER | FRONTEND DEV</h2>
                 <h1>VICTOR KANG</h1>
             </div>
             <div className={`content`}>
-                <h3>Open To Work | Available from April onwards</h3>
+                <h3>Open To Work | Available from May onwards</h3>
                 <h2 className='typewriter'>Hi, Welcome to my Portfolio. I am in my fourth year at National University of Malaysia (UKM) studying Bachelor Degree of Software Engineering Multimedia. Feel free to take a look at my software and design projects!</h2>
                 <br></br>
                 <a href='./resume/VictorKangKaiSheng_Resume_April2023onwards.pdf' id='resumebtn' className={`${aboutMeIsVisible ? 'fadeIn-late' : ''}`}>View Resume</a>
--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,53 +1,52 @@
-import React, { useEffect, useRef } from 'react'
-import AboutMe from './AboutMe';
-import AcademicProject from './AcademicProjects';
-import ContactMe from './ContactMe';
-import DesignProject from './DesignProjects';
-import Header from './Header';
-import './HomeScreen.css'
-import PersonalProjects from './PersonalProjects';
+import React, { useEffect, useRef } from "react";
+import AboutMe from "./AboutMe";
+import AcademicProject from "./AcademicProjects";
+import ContactMe from "./ContactMe";
+import DesignProject from "./DesignProjects";
+import Header from "./Header";
+import "./HomeScreen.css";
+import EcommerceGame from "./ECommerceGame";
 
 const HomeScreen = () => {
-
   const bgRef = useRef(null);
 
   useEffect(() => {
-      const handleScroll = () => {
-          const scrollPos = window.scrollY;
-          const bgElement = bgRef.current;
-          if (bgElement) {
-              bgElement.style.top = `${scrollPos * 0.7}px`;
-          }
-      };
+    const handleScroll = () => {
+      const scrollPos = window.scrollY;
+      const bgElement = bgRef.current;
+      if (bgElement) {
+        bgElement.style.top = `${scrollPos * 0.7}px`;
+      }
+    };
 
-      window.addEventListener('scroll', handleScroll);
+    window.addEventListener("scroll", handleScroll);
 
-      return () => {
-          window.removeEventListener('scroll', handleScroll);
-      };
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
   }, []);
 
   return (
-    <div className='home'>
-
-      <img src="./background.png" className="bg-img" ref={bgRef}></img>
+    <div>
+      <img
+        src="./background4.png"
+        className="-z-10 absolute top-0 left-0 w-full bg-cover select-none"
+        ref={bgRef}
+      ></img>
 
       <Header />
 
       <AboutMe />
 
+      <EcommerceGame />
+
       <ContactMe />
 
       <AcademicProject />
 
-      <PersonalProjects />
-
       <DesignProject />
-
-      <ContactMe />
-
     </div>
-  )
-}
+  );
+};
 
 export default HomeScreen;
