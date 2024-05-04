a594f44eeb39b2966cbb326bd9e6384bd72dfb0c
Victor Kang Kai Sheng <66403541+victorkang516@users.noreply.github.com>
2024-05-04 22:17:58 +0800

    Header

---
+++ src/screens/header/Header.js
@@ -0,0 +1,19 @@
+import React from "react";
+import NavItem from "./NavItem";
+
+const Header = () => {
+  return (
+    <div className="flex flex-row justify-between items-center text-white p-4 w-[80vw] mx-auto">
+      <a href="" className="text-[40px] font-freeman">
+        VK
+      </a>
+      <div className="flex flex-row justify-around gap-8 h-fit">
+        <NavItem href="#about-me">About Me</NavItem>
+        <NavItem href="#projects">Projects</NavItem>
+        <NavItem href="#contact-me">Contact Me</NavItem>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
