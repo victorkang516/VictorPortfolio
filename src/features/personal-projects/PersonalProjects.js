71b20028cb0cc5fecfd5b74d155e9cb309c6e749
Victor Kang Kai Sheng <victor.ks.kang@lpstech.com>
2024-08-06 15:15:59 +0800

    Update tutora link

--- src/features/personal-projects/PersonalProjects.js
+++ src/features/personal-projects/PersonalProjects.js
@@ -9,7 +9,7 @@ const PersonalProject = () => {
   return (
     <div className="bg-gray-200 text-black select-none" id="projects">
       <div className="px-10 md:w-5/6 mx-auto py-20">
-        <h1 className="font-freeman font-bold text-[4vw] mb-4">
+        <h1 className="font-freeman font-bold text-[100px] mb-4">
           Personal Projects
         </h1>
         <div className="mb-4">
@@ -19,40 +19,46 @@ const PersonalProject = () => {
         </div>
         <div
           ref={portfolioRef}
-          className={`grid grid-cols-2 gap-4 ${refIsVisible ? "fadeIn" : ""}`}
+          className={`grid grid-cols-1 gap-4 ${refIsVisible ? "fadeIn" : ""}`}
         >
-          <div className="bg-white p-4 rounded-lg w-full h-full group flex flex-col justify-start items-start overflow-hidden transition-transform shadow-md">
-            <div className="h-auto w-auto border-2 border-gray-200">
+          <div className="bg-white p-8 rounded-lg w-full h-full group flex flex-row justify-start items-start overflow-hidden transition-transform shadow-md">
+            <div className="h-auto w-auto border border-gray-200">
               <img
-                src="./img/vizitor/vizitor.png"
+                src="./img/tutora/tutoraThumbnail.png"
                 alt="project"
-                className="object-cover"
+                className="object-cover w-full"
               ></img>
             </div>
-            <div className="flex flex-row items-center gap-4 mt-4">
-              <p className="text-gray-600 font-normal text-xs">
-                Started on 2024/06/01
+
+            <div className="flex flex-col ml-8">
+
+              <div className="flex flex-row items-center gap-4">
+                <p className="text-black font-bold text-3xl my-2">Tutora</p>
+                <div className="border border-blue-400 text-blue-400 text-sm p-1 px-4 rounded-full">
+                  In Progress
+                </div>
+              </div>
+              <p className="text-black mb-2">
+                Tutora is a platform for students and parents to find tuition centers or private tutor that best suit their needs.
               </p>
-              <div className="bg-blue-300 text-white text-xs p-1 px-2 rounded-full">
-                In Progress
+              <div className="flex flex-col text-md font-semibold text-blue-800 mb-8">
+
+                <div>React JS</div>
+                <div>Vite JS</div>
+                <div>Tailwind CSS</div>
+                <div>Figma</div>
+
               </div>
+              <a
+                href="https://tutora-victorkang.netlify.app"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <div className="text-center bg-blue-700 hover:bg-blue-900 text-white font-semibold text-sm p-2 px-8 rounded-full mb-2 w-fit">
+                  View Website
+                </div>
+              </a>
             </div>
-            <div className="flex flex-row items-center gap-4">
-              <p className="text-black font-bold text-xl my-2">Vizitor</p>
-            </div>
-            <p className="text-black mb-2">
-              Vizitor aim to streamline the visitor management process of a
-              manufacturing company.
-            </p>
-            <a
-              href="https://vizitor-victorkang.netlify.app"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              <div className="bg-blue-700 hover:bg-blue-900 text-white font-semibold text-sm p-2 px-4 rounded-full mb-2">
-                View Website
-              </div>
-            </a>
           </div>
         </div>
       </div>
